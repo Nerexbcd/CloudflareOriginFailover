db6ce4c65f5419bd57a6df82b41160c68acdd186
Nerexbcd <me@nerexbcd.dev>
2024-11-15 15:50:44 +0000

    Started Work on Central Server

---
+++ Server/server.js
@@ -0,0 +1,60 @@
+const server         = require("http").createServer();
+const { Server }     = require("socket.io");
+const { instrument } = require("@socket.io/admin-ui");
+
+// Service Start Message
+console.log('\nCloudflare Origin Failover Communication Server');
+console.log('github.com/Nerexbcd/CloudflareOriginFailover');
+console.log('Project by Nerexbcd\n');
+
+
+
+// ------------ Prepare Configs ------------ //
+const config = require('./config')();
+
+// ------------ Prepare Socket.IO Server with CORS ------------ //
+const io = new Server(server, {
+  cors: {
+    origin: config.cors,
+    credentials: true
+  }
+});
+
+// ------------ Prepare and Start Metrics Server ------------ //
+const MetricServer = require('./src/MetricsServer');
+const metric = new MetricServer();
+metric.start();
+
+// ------------ Prepare and Connect to Database ------------ //
+const DataBase = require('./src/Database');
+const { exit } = require("process");
+const db = new DataBase();
+// db.connect();
+
+// Reset currently running nodes
+// db.resetNodes();
+
+// ------------ Define NameSpaces ------------ //
+// require('./src/Namespaces/remoteTester')(io,metric.namespaceEvents,db.namespaces.nodes);
+//require('./src/Namespaces/web')(io,metric.namespaceEvents,db);
+
+// Refuse Main NameSpace Connections
+io.use((socket, next) => {
+  return next(new Error("Not Authorized"));
+});
+
+// ------------ Config for Admin UI ------------ //
+/* instrument(io, {
+  auth: {
+    type: "basic",
+    username: config.webUi_username,
+    password: config.webUi_password // "changeit" encrypted with bcrypt
+  },
+  readonly: config.webUi_readonly,
+  mode: config.webUi_mode,
+}); */
+
+// ------------ Start the Server ------------ //
+server.listen(3000, () => {
+  console.log('socket.io Server listening on *:3000\n');
+});
