4b085345b907129b4a8412c4a3ca19c4aee92ba3
Nerexbcd <me@nerexbcd.dev>
2024-11-20 12:02:45 +0000

    Cleaned Project from unnecessary files

--- Server/Resources/js/album.js
+++
@@ -1,8 +0,0 @@
-function maxSizingAlbumArea() {
-    var header = document.getElementsByTagName("header")[0];
-    var search = document.getElementById("search");
-    var scrollableArea = document.getElementById("scrollableArea");
-    scrollableArea.style.maxHeight = "calc(100vh - (" + header.offsetHeight + "px + 6rem + " + search.offsetHeight + "px))";
-}
-
-maxSizingAlbumArea()
--- Server/Resources/js/scrollableArea.js
+++
@@ -1,3 +0,0 @@
-var header = document.getElementsByTagName("header")[0]
-var scrollableArea = document.getElementById("scrollableArea")
-scrollableArea.style.maxHeight = "calc(100vh - (" + header.offsetHeight + "px + 6rem))";
