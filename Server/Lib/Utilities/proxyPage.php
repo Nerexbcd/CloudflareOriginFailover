23bbee542533654cf69fcb796fde9afacbce9855
Nerexbcd <me@nerexbcd.dev>
2024-11-20 13:38:26 +0000

    Updated to try to fix in Vercel

--- Server/Lib/Utilities/proxyPage.php
+++ Server/Lib/Utilities/proxyPage.php
@@ -2,7 +2,9 @@
 
 function get_proxy_site_page( $url )
 {
-    $options = [
+    $ch = curl_init($url);
+
+/*     $options = [
         CURLOPT_RETURNTRANSFER => true,     // return web page
         CURLOPT_HEADER         => false,     // return headers
         CURLOPT_FOLLOWLOCATION => true,     // follow redirects
@@ -13,8 +15,20 @@ function get_proxy_site_page( $url )
         CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
     ];
 
-    $ch = curl_init($url);
-    // curl_setopt_array($ch, $options);
+    curl_setopt_array($ch, $options); */
+
+
+    curl_setopt(($ch), CURLOPT_RETURNTRANSFER, true);
+    curl_setopt(($ch), CURLOPT_HEADER, false);
+    curl_setopt(($ch), CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt(($ch), CURLOPT_ENCODING, "");
+    curl_setopt(($ch), CURLOPT_AUTOREFERER, true);
+    curl_setopt(($ch), CURLOPT_CONNECTTIMEOUT, 120);
+    curl_setopt(($ch), CURLOPT_TIMEOUT, 120);
+    curl_setopt(($ch), CURLOPT_MAXREDIRS, 10);
+
+    curl_setopt(($ch), CURLOPT_SSL_VERIFYPEER, false);
+
     $remoteSite = curl_exec($ch);
     curl_close($ch);
     return $remoteSite;
